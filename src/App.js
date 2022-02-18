bcc0c07fffc4247b31d92304339ec2077be283f9
danialjankouk <dani.jankook@gmail.com>
2022-02-18 14:13:04 +0330

    Context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Login from './components/Login';
-import {Routes ,Route } from "react-router-dom"
+import React from "react";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Chat from "./components/Chat"
+//context
+import AuthContextProvider from "./context/AuthContextProvider";
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/chat" element={ <Chat/>}/>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 };
---
+++ src/context/AuthContextProvider.js
@@ -0,0 +1,28 @@
+ import React,{useEffect,useState,createContext} from 'react';
+ import { auth } from '../Firebase';
+import { useNavigate} from "react-router-dom"
+//CONTEXT
+ const AuthContext = createContext()
+
+const AuthContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+      if (user) navigate("/chat");
+    });
+  }, [user, navigate]);
+  return (
+    <div>
+      <AuthContext.Provider value={user}>
+        {!loading && children}
+      </AuthContext.Provider>
+    </div>
+  );
+};
+ 
+ export default AuthContextProvider;
